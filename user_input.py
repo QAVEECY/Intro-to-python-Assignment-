00fb9f2940e928d5348777348fd4ed1652eda12e
Garydaniels Karea <kareagarrydan@gmail.com>
2024-10-29 14:36:30 +0300

    Introduction to python Assignment

---
+++ user_input.py
@@ -0,0 +1,6 @@
+name = input('Enter Your name: ')
+age = input('Enter your age: ')
+location = input('Enter your location: ')
+#Hello [name], you are [age] years old and live in [location]."
+
+print(f"Hello {name}, you are {age} years old and live in {location}")
